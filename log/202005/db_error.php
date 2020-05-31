7d6f406db7fb27d83da605192b37e45eb4d5beee
EC2 Default User <ec2-user@ip-172-31-26-226.us-west-1.compute.internal>
2020-05-31 01:09:41 +0000

    init commit

---
+++ log/202005/db_error.php
@@ -0,0 +1,2 @@
+<?php exit;?>	2020-05-31 08:45:12	152.32.108.90	/bbs/install/action=db	0	SQL: errno: 2002, errstr: 连接数据库服务器失败:SQLSTATE[HY000] [2002] Connection refused
+<?php exit;?>	2020-05-31 08:45:40	152.32.108.90	/bbs/install/action=db	0	SQL: errno: 1045, errstr: 连接数据库服务器失败:SQLSTATE[HY000] [1045] Access denied for user 'bbs'@'172.17.0.1' (using password: YES)
